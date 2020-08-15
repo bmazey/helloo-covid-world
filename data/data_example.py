42408862242175f6af8dfb93fcf88832f34f540e
Brandon <b.mazey@nyu.edu>
2020-08-15 15:21:32 -0400

    working on sqlalchemy!

--- data/data_example.py
+++ data/data_example.py
@@ -1,3 +1,5 @@
+import pandas
+
 from sqlalchemy import Column, Integer, Float, Date, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
@@ -8,9 +10,10 @@ Base = declarative_base()
 
 
 # here is our SQLAlchemy database which contains test center location data
-class CovidDb(Base):
-    __tablename__ = 'test-centers'
+class TestingLocation(Base):
+    __tablename__ = 'testing-locations'
     __table_args__ = {'sqlite_autoincrement': True}
+    id = Column(Integer, primary_key=True, nullable=False)
     health_center_name = Column(String)
     operated_by = Column(String)
     street_address = Column(String)
@@ -18,4 +21,14 @@ class CovidDb(Base):
     state = Column(String)
     zip_code = Column(String)
     telephone = Column(String)
-    
+
+
+def create_database(file):
+    engine = create_engine('sqlite:///covid.db')
+    Base.metadata.create_all(engine)
+
+    df = pandas.read_csv(file)
+    df.to_sql(con=engine, index_label='id', name=TestingLocation.__tablename__, if_exists='replace')
+
+    session = sessionmaker()
+    session.configure(bind=engine)
