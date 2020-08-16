5894c1887977a9d5b06791fd2e47a77446de928d
AngelinaAdamiec <41345265+AngelinaAdamiec@users.noreply.github.com>
2020-08-16 14:33:26 -0500

    Added a forms page

---
+++ forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class TestingLocationForm(FlaskForm):
+    health_center_name = StringField('Health Center Name', validators=[DataRequired()])
+    operated_by = StringField('Operated by', validators=[DataRequired()])
+    street_address = StringField('Street Address', validators=[DataRequired()])
+    city = StringField('City', validators=[DataRequired()])
+    state = StringField('State', validators=[DataRequired()])
+    zip_code = StringField('Zip Code', validators=[DataRequired()])
+    telephone_number = StringField('Telephone Number', validators=[DataRequired()])
+    website = StringField('Website', validators=[DataRequired()])
+    test_status = StringField('Test Status', validators=[DataRequired()])
+    telehealth = StringField('TeleHealth', validators=[DataRequired()])
+    description = StringField('Description', validators=[DataRequired()])
